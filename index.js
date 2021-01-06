2669ddfd3d041b345d8a668654a71d0f0604d496
JunkiMin <50887481+JunkiMin@users.noreply.github.com>
2021-01-07 00:29:44 +0900

    210107_wetube
    210107_wetube

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("hi")
+
+import express from "express";
+// var express = require('express');
+
+var app = express();
+
+const PORT = 4000;
+
+
+//GET PUT POST  DELETE
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+
+function handdleHome(req,res,next){
+    // console.log(req);
+    res.send("Hello2");
+    console.log('hi from home');
+}
+
+
+const handleProfile = (req,res,next) => res.send("you are on my profile");
+// ▲ //function handleProfile(req,res){
+//     res.send("you are on my profile");
+// }
+
+const betweenHome = (req,res,next) => {
+    
+    console.log("i'm between")
+    // console.log(next)
+    next();
+
+
+};
+
+app.get("/", handdleHome);
+app.use(betweenHome);
+app.get("/profile",handleProfile);
+//aplication  get("directory(위치)","보낼놈(주로 CSS html임")
+
+app.listen(PORT, handleListening);
