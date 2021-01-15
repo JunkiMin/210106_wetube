bd910ade1a25707f1fabeb9d9e9e8f4d9ba4d862
JunkiMin <junki.min.us@gmail.com>
2021-01-15 18:48:18 +0900

    210115_weture_V2

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,20 @@
 //import {videoData} from "../db"
 import route from "../routes"
+import Video from "../models/Video"
 
 
 
-export const home = (req,res)=>res.render("home",{pageTitle : "home", videoData});
+export const home = async(req,res)=>{
+    
+    try{ //try and Catch를 쓰는이유 조금더 자세히 생가갷보기
+        const videoData = await Video.find({});
+        console.log(videoData);
+    }catch(error){
+        console.log(error);
+        res.render("home",{pageTitle : "home", videoData});
+    }
 
+};
 
 
 
@@ -26,13 +36,21 @@ export const videos = (req,res) => res.render("videos",{pageTitle : "videos"});
 
 export const getUpload = (req,res) => res.render("upload",{pageTitle : "upload"});
 
-export const postUpload = (req,res) => {
+export const postUpload = async(req,res) => {
     const {
-        body:{file,title,description}
+        body: { title,description},
+        file: {path}//:{file,title,description}
     }=req;
+
+const newVideo =await Video.create({
+    fileUrl:path,
+    title,
+    description
+})
  //To do: Upload and save Video
-    console.log("videoDetail");
-    res.redirect(route.videoDetail(1001));
+   // console.log(body.title,file.path)
+    console.log(newVideo)
+    res.redirect(route.videoDetail(newVideo.id));
 
 };
 
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,13 +6,14 @@ import { videos,
         getUpload,
         postUpload
        } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 import route from "../routes";
 
 const videoRouter = express.Router();
 
 videoRouter.get( route.home,videos );  //  한번 라우팅 거쳐옴
 videoRouter.get( route.upload,getUpload ); //  한번 라우팅 거쳐옴 /videos/upoad
-videoRouter.post( route.upload,postUpload ); //  한번 라우팅 거쳐옴 /videos/upoad
+videoRouter.post( route.upload,uploadVideo,postUpload ); //  한번 라우팅 거쳐옴 /videos/upoad
 videoRouter.get( route.videoDetail(),videoDetail ); // /videos/vdetail
 videoRouter.get( route.editVideo,editVideo ) ; // /videos/user :id/edit
 videoRouter.get( route.deleteVideo,deleteVideo ) ; // videos/user :id/delete
